51027f2735898b0f549427873e77032f4154cb45
bhaktishinde123 <153122922+bhaktishinde123@users.noreply.github.com>
2024-07-13 11:22:19 +0530

    Add files via upload

---
+++ Dis.cs
@@ -0,0 +1,14 @@
+class Dis
+{
+	static string var="kavya";
+	static string var1="riya";
+	static string var2="nupur";
+	public static void Main(string[] args)
+	{
+		//Dis obj= new Dis();
+		System.Console.WriteLine("{0} myself",var2);
+	 	System.Console.WriteLine(var);
+		System.Console.WriteLine(var1);
+	}
+	
+}
